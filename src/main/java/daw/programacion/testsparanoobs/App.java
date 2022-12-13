9b023da696a4f35d2a6d1c288697dab08f5a5b3b
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-12-13 20:56:31 +0100

    commit inicial

---
+++ src/main/java/daw/programacion/testsparanoobs/App.java
@@ -0,0 +1,13 @@
+package daw.programacion.testsparanoobs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
