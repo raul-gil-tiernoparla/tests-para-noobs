dfeffcb612ef78dcbbe376038467433ea3dd5102
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-12-13 21:42:48 +0100

    añado otro test unitario cincoMasSieteEsDoce

--- src/test/java/daw/programacion/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -7,9 +7,19 @@ public class CalculadoraTest {
 
     @Test
     public void dosSumarDosEsCuatro() {
-        
+
         int expected = 4;
         int numero1 = 2, numero2 = 2;
+        Calculadora calc = new Calculadora();
+
+        assertEquals(expected, calc.sumar(numero1, numero2));
+    }
+
+    @Test
+    public void cincoSumarSieteEsDoce() {
+
+        int expected = 12;
+        int numero1 = 5, numero2 = 7;
         Calculadora calc = new Calculadora();
 
         assertEquals(expected, calc.sumar(numero1, numero2));
