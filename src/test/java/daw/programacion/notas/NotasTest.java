8d8c301781eea9eaf8268ba8a9e700c0c6c7bf2f
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-12-16 21:20:55 +0100

    añado @beforeeach y @aftereach para crear el objeto

--- src/test/java/daw/programacion/notas/NotasTest.java
+++ src/test/java/daw/programacion/notas/NotasTest.java
@@ -2,16 +2,29 @@ package daw.programacion.notas;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NotasTest {
+
+    Notas notas;
+
+    @BeforeEach
+    void crearObjetoNotas() {
+        notas = new Notas();
+    }
+
+    @AfterEach
+    void borrarObjetoNotas() {
+        notas = null;
+    }
 
     @Test
     public void notaNegativaNoPuedeSer() {
         String expected = "La nota no puede ser menor de 0 o mayor de 10";
         double nota = -5.4;
-        Notas notas = new Notas();
-        assertEquals(expected, notas.notaATexto(nota),"Nota negativa no puede ser evaluada");
+        assertEquals(expected, notas.notaATexto(nota), "Nota negativa no puede ser evaluada");
     }
 
 }
